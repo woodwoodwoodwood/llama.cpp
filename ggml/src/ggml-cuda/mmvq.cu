8048d9893d7b708f08aff4b9e73082445f13b496
cakejiang <cakejiang@tencent.com>
2026-07-07 14:39:11 +0800

    add GSQ2 to mul_mat_vec_q_switch_type in mmvq.cu

--- ggml/src/ggml-cuda/mmvq.cu
+++ ggml/src/ggml-cuda/mmvq.cu
@@ -990,6 +990,12 @@ static void mul_mat_vec_q_switch_type(
                  nchannels_x, nchannels_y, nchannels_dst, stride_channel_x, stride_channel_y, stride_channel_dst,
                  nsamples_x, nsamples_dst, stride_sample_x, stride_sample_y, stride_sample_dst, ids_stride, stream);
             break;
+        case GGML_TYPE_GSQ2:
+            mul_mat_vec_q_switch_ncols_dst<GGML_TYPE_GSQ2>
+                (vx, vy, ids, fusion, dst, ncols_x, nrows_x, ncols_dst, stride_row_x, stride_col_y, stride_col_dst,
+                 nchannels_x, nchannels_y, nchannels_dst, stride_channel_x, stride_channel_y, stride_channel_dst,
+                 nsamples_x, nsamples_dst, stride_sample_x, stride_sample_y, stride_sample_dst, ids_stride, stream);
+            break;
         case GGML_TYPE_Q4_0:
             mul_mat_vec_q_switch_ncols_dst<GGML_TYPE_Q4_0>
                 (vx, vy, ids, fusion, dst, ncols_x, nrows_x, ncols_dst, stride_row_x, stride_col_y, stride_col_dst,
