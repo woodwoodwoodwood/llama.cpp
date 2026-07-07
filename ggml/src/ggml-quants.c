288557e89679f992f50ebd1846d1fdd652695438
cakejiang <cakejiang@tencent.com>
2026-07-07 14:30:07 +0800

    add GSQ2 to ggml_validate_row_data switch
    validate_row_data() had no GSQ2 case, falling to default 'invalid type'
    during llama-quantize validation. GSQ2 uses the same f16-scale validation
    as Q1_0/Q4_0 (VALIDATE_ROW_DATA_D_F16_IMPL).

--- ggml/src/ggml-quants.c
+++ ggml/src/ggml-quants.c
@@ -5535,6 +5535,10 @@ bool ggml_validate_row_data(enum ggml_type type, const void * data, size_t nbyte
             {
                 VALIDATE_ROW_DATA_D_F16_IMPL(block_q1_0, data, nb);
             } break;
+        case GGML_TYPE_GSQ2:
+            {
+                VALIDATE_ROW_DATA_D_F16_IMPL(block_gsq2, data, nb);
+            } break;
         case GGML_TYPE_Q4_0:
             {
                 VALIDATE_ROW_DATA_D_F16_IMPL(block_q4_0, data, nb);
